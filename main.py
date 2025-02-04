8d06e27d3b83b2225a7a7dabb06b159306203efd
robCampbell209 <roberthcampbell01@gmail.com>
2025-02-04 23:57:43 +0000

    adding wait after solving before visual

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ if __name__=="__main__":
 
     if solvable(start, goal):
         solution = solve(start, goal)
+        input("Solving complete - press enter to view solution...")
         for state in solution.path():
             os.system("cls")
             printState(state)
