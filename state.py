8518d64814a828c674b7cf9bd432e79bce899b0e
robCampbell209 <roberthcampbell01@gmail.com>
2025-02-04 21:52:12 +0000

    initial commit

---
+++ state.py
@@ -0,0 +1,28 @@
+import random
+
+def heuristicPH(x):
+    return random.randint(0, 100)
+
+class State:
+    def __init__(self, state):
+        self.prevState = None
+        self.state = state
+        self.f = 0
+        self.g = 0
+        self.h = heuristicPH(state)
+
+    def setDepth(self, g):
+        self.g = g
+        self.f = self.g + self.h
+
+    def createChild(self, newState):
+        newState = State(newState)
+        newState.prevState = self
+        newState.setDepth(self.g + 1)
+        return newState
+    
+    def path(self):
+        if self.prevState == None:
+            return self.state
+        else:
+            return self.prevState.path() + self.state
